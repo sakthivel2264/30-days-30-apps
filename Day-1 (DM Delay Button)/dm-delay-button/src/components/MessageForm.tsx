e4185a9ec3992d95bcf7897a6ee87125342d2a69
sakthivel2264 <sakthivelpandiyan2264@gmail.com>
2025-08-01 12:01:49 +0530

    first commit

---
+++ Day-1 (DM Delay Button)/dm-delay-button/src/components/MessageForm.tsx
@@ -0,0 +1,69 @@
+import { Textarea } from "./ui/textarea"
+import { Button } from "./ui/button"
+import { useState } from "react"
+import { Input } from "./ui/input"
+
+const MessageForm = () => {
+
+    const [message, setMessage] = useState<string>("")
+    const [delay, setDelay] = useState<number>(0)
+    const [isSending, setIsSending] = useState<boolean>(false)
+    const [timerId, setTimerId] = useState<NodeJS.Timeout | null>(null)
+    const [sentMessage, setSentMessage] = useState<string>("")
+
+    const handleSend = () => {
+        setIsSending(true)
+        const id = setTimeout(() => {
+            setSentMessage(message)
+            setMessage("")
+            setIsSending(false)
+        }, delay * 1000)
+        setTimerId(id)
+    }
+
+    const handleCancel = () => {
+        if (timerId) {
+            clearTimeout(timerId)
+            setIsSending(false)
+        }
+    }
+
+
+
+  return (
+    <div className='max-w-md mx-auto p-4 mt-40 bg-white border-2 shadow-md rounded-lg '>
+        <h2 className='text-2xl text-center p-4 font-bold'>DM Delay Button</h2>
+        <Textarea
+          placeholder='Type your message here...'
+          className='w-full h-32 mb-4'
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+        />
+        <Input
+          type='text'
+          placeholder='Enter delay in seconds'
+          className='w-full mb-4'
+          onChange={(e) => setDelay(Number(e.target.value))}
+          value={delay}
+          disabled={isSending}
+        />
+        {!isSending ? 
+        <Button className="w-full" onClick={handleSend}>
+          Send Message with Delay
+        </Button>
+        :
+        <Button className="w-full" variant="destructive" onClick={handleCancel}>
+          Cancel Sending...
+        </Button>
+        }   
+
+        {sentMessage && (
+          <div className='mt-4 p-2 bg-green-100 text-green-800 rounded'>
+            <p>Message Sent: {sentMessage}</p>
+          </div>
+        )}
+    </div>
+  )
+}
+
+export default MessageForm
---
+++ Day-1 (DM Delay Button)/dm-delay-button/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+
+import MessageForm from '@/components/MessageForm'
+
+const Home = () => {
+  return (
+    <div className='min-h-screen bg-gray-100 flex items-start justify-center'>
+      <MessageForm/>
+    </div>
+  )
+}
+
+export default Home
